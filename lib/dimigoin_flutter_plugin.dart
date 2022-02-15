83f88065758ce4c32bad3e824be95580e700a8b6
doch13_ <0922happy65@gmail.com>
2022-02-15 21:16:18 +0900

    Initial commit

---
+++ lib/dimigoin_flutter_plugin.dart
@@ -0,0 +1,7 @@
+library dimigoin_flutter_plugin;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/dimigoin_flutter_plugin_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:dimigoin_flutter_plugin/dimigoin_flutter_plugin.dart';
+
+void main() {
+}
